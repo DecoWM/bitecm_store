b313e11a097bc200bfcf6c62261f26e34e3b15cf
Diego Chinga Olivera <diego.chinga.olivera@gmail.com>
2017-12-11 19:33:07 -0500

    table sessions

--- database/scripts/bitel-store_11_12_2017.sql
+++ database/scripts/bitel-store_11_12_2017.sql
@@ -20,6 +20,17 @@ DROP DATABASE IF EXISTS `bitel_ecommerce`;
 CREATE SCHEMA IF NOT EXISTS `bitel_ecommerce` DEFAULT CHARACTER SET utf8 ;
 USE `bitel_ecommerce` ;
 
+-- -----------------------------------------------------
+-- Table `bitel_ecommerce`.`sessions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(10) UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_agent` text COLLATE utf8mb4_unicode_ci,
+  `payload` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_activity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Estructura de tabla para la tabla `tbl_affiliation`
