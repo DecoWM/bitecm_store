b17d70f4aa9387dbd7459f9202a4498400d3059a
Diego Chinga Olivera <diego.chinga.olivera@gmail.com>
2018-02-07 20:40:07 -0500

    chrome fix for order buton

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -51,15 +51,11 @@ const form = new Vue({
     affiliation: ''
   },
   methods: {
-    validateInfoCliente(event) {
-      event.preventDefault();
+    validateInfoCliente() {
       this.$validator.validateAll().then((result) => {
-        var sub = parseInt($('#submitOrder').data('sub'));
-        if (result && !sub) {
-          $('#submitOrder').data('sub','1');
+        if (result) {
           this.$refs.orderform.submit();
-        } else {
-          $('#submitOrder').data('sub','0');
+          return;
         }
       });
     },
