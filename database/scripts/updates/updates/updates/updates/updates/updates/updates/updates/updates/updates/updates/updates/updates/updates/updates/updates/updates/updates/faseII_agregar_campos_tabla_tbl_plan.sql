29eebcb583cbae150417b4216ea376659e2fd266
Diego Chinga Olivera <diego.chinga.olivera@gmail.com>
2018-04-16 18:18:28 -0500

    removing recursive mess

--- database/scripts/updates/updates/updates/updates/updates/updates/updates/updates/updates/updates/updates/updates/updates/updates/updates/updates/updates/updates/faseII_agregar_campos_tabla_tbl_plan.sql
+++
@@ -1,4 +0,0 @@
-USE `bitel_ecommerce` ;
-
-ALTER TABLE tbl_plan ADD COLUMN vende_en_chip TINYINT(4) DEFAULT 0 AFTER active;
-ALTER TABLE tbl_plan ADD COLUMN disponible_en_chip TINYINT(1) DEFAULT 0 AFTER vende_en_chip;
