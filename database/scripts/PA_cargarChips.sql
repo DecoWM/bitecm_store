abfe7bb533af2edcaf6f00ddaa75384433af38b4
Diego Chinga Olivera <diego.chinga.olivera@gmail.com>
2018-04-18 22:47:40 -0500

    cargar chips cat 4

--- database/scripts/PA_cargarChips.sql
+++ database/scripts/PA_cargarChips.sql
@@ -4,24 +4,18 @@ USE `bitel_ecommerce`$$
 
 CREATE PROCEDURE PA_cargarChips()
 BEGIN
-DECLARE _category_id INT(11);
 DECLARE _brand_id INT(11);
 DECLARE _product_id INT(11);
-
--- insertar la categoria del producto chip
-INSERT INTO tbl_category (category_name, category_slug, allow_variation, weight, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, active)
-VALUES ('Chips', 'chips', 1, 1, NOW(), NULL, NULL, 1, NULL, NULL, 1);
 
 -- insertar en tabla brand
 INSERT INTO tbl_brand (brand_name, brand_slug, weight, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, active)
 VALUES('Bitel','Bitel',1,NOW(),NULL,NULL,1,NULL,NULL,1);
 
-SELECT MAX(category_id) INTO _category_id FROM tbl_category;
 SELECT MAX(brand_id) INTO _brand_id FROM tbl_brand;
 
 -- insertar el producto 
 INSERT INTO tbl_product (category_id, brand_id, product_model, product_image_url, product_keywords, product_price, product_description, product_general_specifications, product_data_sheet, product_external_memory, product_internal_memory, product_screen_size, product_camera_1, product_camera_2, product_camera_3, product_camera_4, product_processor_name, product_processor_power, product_processor_cores, product_band, product_radio, product_wlan, product_bluetooth, product_os, product_gps, product_battery, product_slug, product_tag, product_priority, created_at, updated_at, deleted_at, publish_at, created_by, updated_by, deleted_by, publish_by, active)
-VALUES (_category_id, _brand_id, 'iChip','productos/chipbitel.jpg',NULL,1.00,NULL,NULL,'data_sheets/Ficha_tecnica_SAMSUNG_GALAXY_J7 PRIME.pdf',128,4,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'chip-bitel',NULL,1,NOW(),NULL,NULL,NOW(),1,NULL,NULL,1,1);
+VALUES (4, _brand_id, 'iChip','productos/chipbitel.jpg',NULL,1.00,NULL,NULL,'data_sheets/Ficha_tecnica_SAMSUNG_GALAXY_J7 PRIME.pdf',128,4,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'chip-bitel',NULL,1,NOW(),NULL,NULL,NOW(),1,NULL,NULL,1,1);
 
 -- insertar variaciones del chip
 -- variation_type_id: 1-2 1=postpago, 2=prepago
