552adf6803d21a21f4c624e5ee01a36f9b533c98
Diego Chinga Olivera <diego.chinga.olivera@gmail.com>
2018-05-16 22:56:11 -0500

    ws renovación

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -256,8 +256,9 @@ class OrderController extends Controller
 
       if ($response->error == '0') {
         if (!empty($response->original)) {
-          $original = simplexml_load_string($response->original);
-          Log::info('bitelSoapGW.gwOperation.original: ', (array) $original);
+          Log::info('bitelSoapGW.gwOperation.original: ', $response->original);
+          $original = json_decode(json_encode(simplexml_load_string($response->original)));
+          Log::info('bitelSoapGW.gwOperation.original: ', $original);
           if ($original->return->code != '0') {
             if ($original->return->checkedSubscriber->isExist != 'false') {
               return false; 
