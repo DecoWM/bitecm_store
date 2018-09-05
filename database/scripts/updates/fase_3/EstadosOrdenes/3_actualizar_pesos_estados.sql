d4a3534267b974474d05ac2bdca321db25592575
Carlos Euribe <carlos.euribe@forceclose.pe>
2018-09-05 14:24:35 -0500

    Fase 3 Bitel

--- database/scripts/updates/fase_3/EstadosOrdenes/3_actualizar_pesos_estados.sql
+++ database/scripts/updates/fase_3/EstadosOrdenes/3_actualizar_pesos_estados.sql
@@ -2,7 +2,7 @@ USE `bitel_ecommerce`;
 
 UPDATE tbl_order_status SET weight = 1, weight2 = 1 WHERE order_status_name = 'Pendiente';
 UPDATE tbl_order_status SET weight = 2, weight2 = 3 WHERE order_status_name = 'Procesado';
-UPDATE tbl_order_status SET weight = 4, weight2 = 0 WHERE order_status_name = 'En Envío';
+UPDATE tbl_order_status SET weight = 4, weight2 = 0 WHERE order_status_id = 3;
 UPDATE tbl_order_status SET weight = 7, weight2 = 0 WHERE order_status_name = 'Cancelado';
 UPDATE tbl_order_status SET weight = 5, weight2 = 0 WHERE order_status_name = 'Completado';
 UPDATE tbl_order_status SET weight = 3, weight2 = 0 WHERE order_status_name = 'Programado';
